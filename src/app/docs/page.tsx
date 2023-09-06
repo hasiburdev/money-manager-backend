1e784b2d95823eb9fa86f00f3ae208dec8ad06e1
Hasibur Rahman <hasiburdev@gmail.com>
2023-09-06 20:08:29 +0600

    Disable swagger

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,13 +1,14 @@
-"use client";
+// "use client";
 
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import { docsData } from "./swagger";
+// import SwaggerUI from "swagger-ui-react";
+// import "swagger-ui-react/swagger-ui.css";
+// import { docsData } from "./swagger";
 
 const Docs = () => {
   return (
     <div>
-      <SwaggerUI spec={docsData} />
+      {/* <SwaggerUI spec={docsData} /> */}
+      <h1>Swagger Docs</h1>
     </div>
   );
 };
